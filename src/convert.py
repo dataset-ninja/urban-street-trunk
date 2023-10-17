e2f13f742e00b9e16c74938b92cf2fd51dfef976
erastov-alex <erastov.alex@gmail.com>
2023-10-17 12:30:22 +0300

    upd convert

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,16 @@
-import supervisely as sly
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
 import shutil
+from urllib.parse import unquote, urlparse
 
+import numpy as np
+import supervisely as sly
+from cv2 import connectedComponents
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import get_file_name, get_file_size
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -69,17 +73,145 @@ def count_files(path, extension):
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    batch_size = 20
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
-
-    # ... some code here ...
-
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
-
-    # return project
+    classes_path = os.path.join("trunk","VOCdevkit","VOC2012","class_names.txt")
+    images_path = os.path.join("trunk","VOCdevkit","VOC2012","JPEGImages")
+    masks_path =  os.path.join("trunk","VOCdevkit","VOC2012","SegmentationClass")
+    split_folder =  os.path.join("trunk","VOCdevkit","VOC2012","ImageSets","Segmentation")
+    images_ext = ".jpg"
+    masks_ext = ".png"
 
 
+    def get_unique_colors(img):
+        unique_colors = []
+        img = img.astype(np.int32)
+        h, w = img.shape[:2]
+        colhash = img[:, :, 0] * 256 * 256 + img[:, :, 1] * 256 + img[:, :, 2]
+        unq, unq_inv, unq_cnt = np.unique(colhash, return_inverse=True, return_counts=True)
+        indxs = np.split(np.argsort(unq_inv), np.cumsum(unq_cnt[:-1]))
+        col2indx = {unq[i]: indxs[i][0] for i in range(len(unq))}
+        for col, indx in col2indx.items():
+            if col != 0:
+                unique_colors.append((col // (256**2), (col // 256) % 256, col % 256))
+
+        return unique_colors
+
+
+    def create_ann(image_path):
+        labels = []
+
+        image_name = get_file_name(image_path)
+        class_name = image_name.split("_trunk")[0]
+        class_name = mask_name_to_class_name.get(class_name, class_name)
+        class_name_lower = class_name.lower()
+        class_name_lower_corr = '_'.join(class_name_lower.split(' '))
+        obj_class = meta.get_obj_class(class_name_lower_corr)
+        tags = [sly.Tag(class_tag, value=class_name_lower_corr)]
+
+        mask_path = os.path.join(masks_path, image_name + masks_ext)
+
+        mask_np = sly.imaging.image.read(mask_path)
+        img_height = mask_np.shape[0]
+        img_wight = mask_np.shape[1]
+        unique_colors = get_unique_colors(mask_np)
+        for color in unique_colors:
+            mask = np.all(mask_np == color, axis=2)
+            ret, curr_mask = connectedComponents(mask.astype("uint8"), connectivity=8)
+            for i in range(1, ret):
+                obj_mask = curr_mask == i
+                bitmap = sly.Bitmap(data=obj_mask)
+                if bitmap.area > 100:
+                    label = sly.Label(bitmap, obj_class)
+                    labels.append(label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=tags)
+
+
+    class_name_to_mask_name = {
+        "trunk of Acer palmatum": "Acer palmatum",
+        "trunk of Aesculus chinensis": "Aesculus chinensis",
+        "trunk of Albizia julibrissin": "Albizia julibrissin",
+        "trunk of Camptotheca acuminata": "Camptotheca acuminata",
+        "trunk of Cedrus deodara": "Cedrus deodara",
+        "trunk of Celtis sinensis": "Celtis sinensis",
+        "trunk of Cinnamomum camphora (Linn) Presl": "Cinnamomum camphora (Linn) Presl",
+        "trunk of Elaeocarpus decipiens": "Elaeocarpus decipiens",
+        "trunk of Flowering cherry": "Flowering cherry",
+        "trunk of Ginkgo biloba": "Ginkgo biloba",
+        "trunk of Koelreuteria paniculata": "Koelreuteria paniculata",
+        "trunk of Lagerstroemia indica": "Lagerstroemia indica",
+        "trunk of Liquidambar formosana": "Liquidambar formosana",
+        "trunk of Liriodendron chinense": "Liriodendron chinense",
+        "trunk of Magnolia grandiflora L.": "Magnolia grandiflora L",
+        "trunk of Magnolia liliflora Desr": "Magnolia liliflora Desr",
+        "trunk of Malushalliana": "Malushalliana",
+        "trunk of Metasequoia glyptostroboides": "Metasequoia glyptostroboides",
+        "trunk of Michelia chapensis": "Michelia chapensis",
+        "trunk of Osmanthus fragrans": "Osmanthus fragrans",
+        "trunk of Photinia serratifolia": "Photinia serratifolia",
+        "trunk of Platanus": "Platanus",
+        "trunk of Populus L.": "Populus L",
+        "trunk of Prunus cerasifera f. atropurpurea": "Prunus cerasifera f. atropurpurea",
+        "trunk of Salix babylonica": "Salix babylonica",
+        "trunk of Sapindus saponaria": "Sapindus saponaria",
+        "trunk of Styphnolobium japonicum": "Styphnolobium japonicum",
+        "trunk of Triadica sebifera": "Triadica sebifera",
+        "trunk of Zelkova serrata": "Zelkova serrata",
+    }
+
+    mask_name_to_class_name = dict((v, k) for k, v in class_name_to_mask_name.items())
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta()
+
+    with open(classes_path) as f:
+        content = f.read().split("\n")
+
+        for idx, class_name in enumerate(content):
+            if idx != 0:
+                mask_path = os.path.join(
+                    masks_path,
+                    class_name_to_mask_name.get(class_name, class_name) + "_trunk_1 (1).png",
+                )
+                mask_np = sly.imaging.image.read(mask_path)
+                unique_colors = get_unique_colors(mask_np)
+                class_name_lower = class_name.lower()
+                class_name_lower_corr = '_'.join(class_name_lower.split(' '))
+                obj_class = sly.ObjClass(class_name_lower_corr, sly.Bitmap, color=unique_colors[0])
+                meta = meta.add_obj_class(obj_class)
+
+    tag_metas= []
+    class_tag = sly.TagMeta(name="classification tag",value_type=sly.TagValueType.ANY_STRING)
+    tag_metas.append(class_tag)
+    meta = meta.add_tag_metas(tag_metas)
+    api.project.update_meta(project.id, meta.to_json())
+
+
+    for split_file in os.listdir(split_folder):
+        if get_file_ext(split_file) != ".txt":
+            continue
+        ds_name = get_file_name(split_file)
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+        split_path = os.path.join(split_folder, split_file)
+
+        with open(split_path) as f:
+            content = f.read().split("\n")
+            images_names = [im_name + images_ext for im_name in content if len(im_name) > 0]
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+        for img_names_batch in sly.batched(images_names, batch_size=batch_size):
+            images_pathes_batch = [
+                os.path.join(images_path, image_name) for image_name in img_names_batch
+            ]
+
+            img_infos = api.image.upload_paths(dataset.id, img_names_batch, images_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            anns_batch = [create_ann(image_path) for image_path in images_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns_batch)
+
+            progress.iters_done_report(len(img_names_batch))
+
+    return project
