0c61d80fb0dc9cdf4239530c16a115ca096a5476
erastov-alex <erastov.alex@gmail.com>
2023-10-18 12:34:00 +0300

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Urban Street: Trunk"
 PROJECT_NAME_FULL: str = "Tree Dataset of Urban Street: Trunk"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
